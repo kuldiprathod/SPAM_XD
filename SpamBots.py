c2601fb271cb3943df56828c2cfd2e996e469450
kuldiprathod <96421875+kuldiprathod@users.noreply.github.com>
2022-01-26 20:55:34 +0530

    Update SpamBots.py

--- SpamBots.py
+++ SpamBots.py
@@ -329,7 +329,7 @@ async def _(e):
             event = await e.reply(text, parse_mode=None, link_preview=None)
             try:
                 await e.client(functions.channels.JoinChannelRequest(channel=bc))
-                await event.edit("ℂℍ𝔸𝕃𝕆 𝔾𝔸ℕ𝔻 𝕄𝔸ℝℕ𝔼 𝕁𝕆𝕀ℕ ℍ𝕆 𝔾𝔸𝕐𝔸")
+                await event.edit("𝐀𝐀 𝐑𝐀𝐇𝐀 𝐁𝐒𝐃𝐊𝐄 ")
             except Exception as e:
                 await event.edit(str(e))
         else:
@@ -384,7 +384,7 @@ async def _(e):
             event = await e.reply(text, parse_mode=None, link_preview=None )
             try:
                 await event.client(LeaveChannelRequest(bc))
-                await event.edit("𝔹𝔸ℍ𝕌𝕋 𝔾𝔸ℕ𝔻 𝕄𝔸ℝ𝕃𝕀 𝔾ℝ𝕆𝕌ℙ 𝕄𝔸𝕀  ℂℍ𝕆ℝℕ𝔼 𝕂𝔸 𝕋𝕀𝕄𝔼 𝔸𝔸 𝔾𝔸𝕐𝔸")
+                await event.edit("𝐂𝐇𝐀𝐋𝐎 𝐂𝐇𝐔𝐃 𝐆𝐀𝐘𝐀 𝐘𝐄 𝐊𝐈𝐃𝐙𝐙 𝐀𝐁 𝐂𝐇𝐀𝐋𝐎")
             except Exception as e:
                 await event.edit(str(e))   
         else:
@@ -724,7 +724,7 @@ async def ping(e):
         event = await e.reply(text, parse_mode=None, link_preview=None)
         end = datetime.now()
         ms = (end - start).microseconds / 1000
-        await event.edit(f"🤤𝐎𝐏_𝐁𝐀𝐁𝐘!\n`{ms}` ms\n⚔️𝐍𝐎𝐁𝐈𝐓𝐀_𝐒𝐏𝐀𝐌⚔️")
+        await event.edit(f"🤤𝐎𝐏_𝐁𝐀𝐁𝐘!\n`{ms}` ms\n⚔🔥𝐍𝐎𝐁𝐈𝐓𝐀_𝐒𝐏𝐀𝐌🔥")
 
 
 @idk.on(events.NewMessage(incoming=True, pattern=r"\.restart"))
@@ -797,7 +797,7 @@ async def restart(e):
 @ddk.on(events.NewMessage(incoming=True, pattern=r"\.help"))
 async def help(e):
     if e.sender_id in SMEX_USERS:
-        text = "[ 𝐍𝐎𝐁𝐈𝐓𝐀 𝐒𝐏𝐀𝐌𝐁𝐎𝐓 ]\n\n𝐒𝐓𝐀𝐑𝐓 𝐂𝐎𝐌𝐌𝐀𝐍𝐃:\n.ping\n.restart\n\n𝐉𝐎𝐈𝐍 𝐂𝐎𝐌𝐌𝐀𝐍𝐃:\n.join\n.pjoin\n.leave\n\n𝐒𝐏𝐀𝐌 𝐂𝐎𝐌𝐌𝐀𝐍𝐃:\n.spam\n.delayspam\n.bigspam\n.replyraid\n.dreplyraid\n\n𝐒𝐂𝐑𝐀𝐏 𝐂𝐎𝐌𝐌𝐀𝐍𝐃:\n.scrap\n\n𝐅𝐎𝐑 𝐍𝐄𝐄𝐃 𝐀𝐍𝐘 𝐇𝐄𝐋𝐏 𝐀𝐁𝐎𝐔𝐓 𝐒𝐏𝐀𝐌𝐁𝐎𝐓 𝐉𝐎𝐈𝐍 @SH4DOW_SPAMBOTSUPPORT"
+        text = "[ 𝐍𝐎𝐁𝐈𝐓𝐀 𝐒𝐏𝐀𝐌𝐁𝐎𝐓 ]\n\n𝐒𝐓𝐀𝐑𝐓 𝐂𝐎𝐌𝐌𝐀𝐍𝐃:\n.ping\n.restart\n\n𝐉𝐎𝐈𝐍 𝐂𝐎𝐌𝐌𝐀𝐍𝐃:\n.join\n.pjoin\n.leave\n\n𝐒𝐏𝐀𝐌 𝐂𝐎𝐌𝐌𝐀𝐍𝐃:\n.spam\n.delayspam\n.bigspam\n.replyraid\n.dreplyraid\n\n𝐒𝐂𝐑𝐀𝐏 𝐂𝐎𝐌𝐌𝐀𝐍𝐃:\n.scrap\n\n𝐅𝐎𝐑 𝐍𝐄𝐄𝐃 𝐀𝐍𝐘 𝐇𝐄𝐋𝐏 𝐀𝐁𝐎𝐔𝐓 𝐒𝐏𝐀𝐌𝐁𝐎𝐓 𝐉𝐎𝐈𝐍 @NOBITA_SPAMBOTSUPPORT"
         await e.reply(text, parse_mode=None, link_preview=None)
 
 
